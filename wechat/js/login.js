765ca75d37390394d1dbe469587534e47f4f7a15
马特浩倪 <smile.used@hotmail.com>
2016-09-20 18:14:41 +0800

    Update login.js

--- wechat/js/login.js
+++ wechat/js/login.js
@@ -1,5 +1,5 @@
 function LoginSys() {
-        $.post('http://jwzx.usc.edu.cn/Login', { UserName: $("#UserName").val(), Password: $("#Password").val(), Code: $("#ValidateCode").val() },
+        $.post('https://jwzx.usc.edu.cn/Login', { UserName: $("#UserName").val(), Password: $("#Password").val(), Code: $("#ValidateCode").val() },
                function (data) {
                    if (data.type == "1") {
                        window.location = "http://jwzx.usc.edu.cn/Home/Index"
