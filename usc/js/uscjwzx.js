9fd94a9a363be6b45824638cb652a9517d136be9
Mattholy <smile.used@hotmail.com>
2016-09-22 11:21:52 +0800

    利用jquray优化代码

--- usc/js/uscjwzx.js
+++ usc/js/uscjwzx.js
@@ -6,13 +6,13 @@ This is a js file written by Mattholy
 //检查并加载iframe
 function jwzx()
 {
-	document.getElementById('setMsg').innerHTML=document.getElementById('uscjwzx').contentWindow.document.getElementsByTagName('title').innerHTML;
+	//document.getElementById('setMsg').html(document.getElementById('uscjwzx').contentWindow.document.getElementsByTagName('title').innerHTML;
+	
 }
 
 //检测输入数据正确无误，然后输入至iframe相关对象里
 function logindatasend()
 {
-	var y=document.getElementById("mess");
 	var username=document.getElementById("UserName").value;
 	var password=document.getElementById("Password").value;
 	var code=document.getElementById("Code").value;
@@ -21,43 +21,43 @@ function logindatasend()
 	//判断学号
 	if (username==="")
 	{
-		y.innerHTML="你好像忘了输入学号";
+		$("#mess").html("你好像忘了输入学号");
 		UserName.focus();
 		return;
 	}
 	else if (isNaN(username)||username_length!==11)
 	{
-		y.innerHTML="学号格式不对噢";
+		$("#mess").html("学号格式不对噢");
 		UserName.focus();
 		return;
 	}
 	else
 	{
-		y.innerHTML="";
+		$("#mess").html("");
 	}
 	
 	//判断密码
 	if (password==="")
 	{
-		y.innerHTML="你还没有输入密码";
+		$("#mess").html("你还没有输入密码");
 		Password.focus();
 		return
 	}
 	else
 	{
-		y.innerHTML="";
+		$("#mess").html("");
 	}
 	
 	//判断验证码
 	if (code==="")
 	{
-		y.innerHTML="你还没有输入验证码";
+		$("#mess").html("你还没有输入验证码");
 		Code.focus();
 		return
 	}
 	else
 	{
-		y.innerHTML="";
+		$("#mess").html("");
 	}
 	alert(iscomplat);//这是测试
 }
